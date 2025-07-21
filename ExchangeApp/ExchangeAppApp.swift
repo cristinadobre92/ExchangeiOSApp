996bd7e5790c6c896b6c13a089bd69c7aa7053a4
Cristina Dobre <crisfdobre@gmail.com>
2025-07-21 20:50:32 +0200

    Initial Commit

---
+++ ExchangeApp/ExchangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeAppApp.swift
+//  ExchangeApp
+//
+//  Created by Cristina Dobre on 21.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
